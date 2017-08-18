133fb16496dc94dc336f8b0c08293059c3d3e0af
Dennis Yu <dennis.yu@gusto.com>
2017-08-18 15:35:52 -0700

    try other way around

--- lib/gemstash/cache.rb
+++ lib/gemstash/cache.rb
@@ -82,7 +82,7 @@ module Gemstash
   class RedisClient
     extend Forwardable
 
-    def_delegator :@cache, :delete, :del
+    def_delegator :@cache, :del, :delete
     def_delegators :@cache, :get
 
     def initialize(redis_servers)
