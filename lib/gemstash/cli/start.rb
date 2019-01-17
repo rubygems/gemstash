2a628039656d9e54bde6c226aa09ef66962b7ae0
Julia Lee <julia@zenpayroll.com>
2019-01-16 17:56:46 -0800

    Convert puma config options to strings
    This effectively applies commit 76244016ab49fdde403e08e36cb99796a70bb3c1
    When gusto/redis is merged in upstream, then this commit becomes unnecssary.
    For now gusto's gemstash fork is in an unideal inbetween state

--- lib/gemstash/cli/start.rb
+++ lib/gemstash/cli/start.rb
@@ -32,10 +32,6 @@ module Gemstash
         @cli.options[:daemonize]
       end
 
-      def puma_config
-        File.expand_path("../../puma.rb", __FILE__)
-      end
-
       def store_pidfile
         gemstash_env.pidfile = pidfile?
       end
@@ -54,10 +50,22 @@ module Gemstash
 
       def puma_args
         [
-          "--config", puma_config,
-          "--workers", gemstash_env.config[:puma_workers],
-          "--threads", gemstash_env.config[:puma_threads]
+          '--config', puma_config,
+          '--workers', puma_workers,
+          '--threads', puma_threads,
         ]
+      end
+
+      def puma_workers
+        gemstash_env.config[:puma_workers] ? gemstash_env.config[:puma_workers].to_s : '0'
+      end
+
+      def puma_threads
+        gemstash_env.config[:puma_threads] ? gemstash_env.config[:puma_threads].to_s : '0'
+      end
+
+      def puma_config
+        File.expand_path("../../puma.rb", __FILE__)
       end
 
       def daemonize_args
