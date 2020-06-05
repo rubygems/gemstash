a86ed3375650a2260be42ff4a94f2345d4710803
James <spamonlyjames@gmail.com>
2020-06-05 11:45:56 +0700

    Create S3Resource class
    Similar to the original storage.rb resource class

--- lib/gemstash/S3.rb
+++ lib/gemstash/S3.rb
@@ -32,4 +32,29 @@ module Gemstash
     end
 
   end
+  class S3Resource
+    include Gemstash::Env::Helper
+    include Gemstash::Logging
+    attr_reader :name, :folder, :client
+    VERSION = 1
+
+    class VersionTooNew < StandardError
+      def initialize(name, folder, version)
+        super("Gemstash resource version #{Gemstash::S3Resource::VERSION} does " \
+               "not support version #{version} for resource #{name.inspect} " \
+               "found at #{folder}")
+      end
+    end
+    def initialize(folder,name,client)
+      @folder = folder
+      @name = name
+      safe_name = sanitize(@name)
+      digest = Digest::MD5.hexdigest(@name)
+      child_folder = "#{safe_name}-#{digest}"
+      @folder = File.join(@folder, child_folder)
+      @client = client
+      @S3resource = Aws::S3::Resource.new(client: @client).bucket(gemstash_env.config[:bucket_name])
+      @properties = nil
+    end
+  end
 end
