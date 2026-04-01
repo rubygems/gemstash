cce10db59688b7d68e5dfec556f92660fbfe2bff
Caius Durling <dev@caius.name>
2026-04-01 11:00:55 +0100

    Default webserver to running in production environment
    Can be overridden with APP_ENV or RACK_ENV being set to development if we want the development middleware in Sinatra loading or puma running in the development environment for development purposes.

--- lib/gemstash/puma.rb
+++ lib/gemstash/puma.rb
@@ -8,3 +8,4 @@ threads 0, Gemstash::Env.current.config[:puma_threads].to_i
 bind Gemstash::Env.current.config[:bind].to_s
 workers Gemstash::Env.current.config[:puma_workers].to_i unless RUBY_PLATFORM == "java"
 rackup Gemstash::Env.current.rackup
+environment ENV["APP_ENV"] || ENV["RACK_ENV"] || "production"
